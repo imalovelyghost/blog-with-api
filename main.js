38c4f766dfe3f2cd5dafd3a2126808eb44764fc3
imalovelyghost <imalovelyghost13@gmail.com>
2021-04-29 00:17:44 +0200

    all data in my project but only objects, needed to be parsed

--- main.js
+++ main.js
@@ -1,33 +1,55 @@
-// $.ajax({
-//     type: "Get",
-//     url: "https://jsonplaceholder.typicode.com/posts/",
-//     dataType: "json",
-//     success: function(data) {
-//         // $("#post-one").append("<p>Test</p>");
-//         // $("#post-one").append(data);
-//         console.log(data)
-
-//     },
-//     error: function() {
-//         alert("json not found");
-//     }
-// });
-
-// $.get("data/posts.json", function(data) {
-//     $("#post-one").html(data);
-//     alert("Load was performed.");
-// });
-
-//alert("putisima madre")
+//GET DATA POSTS
 $.getJSON("data/posts.json", function(data) {
-    console.log(data)
-    var items = [];
-    $.each(data, function(key, val) {
-        items.push("<li id='" + key + "'>" + val + "</li>");
-    });
+    var posts = [];
 
+    $.each(data, function(key, val) {
+        //console.log(data[1].userId)
+        //console.log(data[1].id)
+        //console.log(data[1].title)
+        //console.log(data[1].body)
+        console.log(data[0])
+
+        posts.push("<div id='" + key + "'>" + val + "</div>");
+        //console.log(items)
+    });
     $("<ul/>", {
         "class": "my-new-list",
-        html: items.join("")
+        html: posts.join("")
+    }).appendTo("body");
+});
+
+//GET DATA USERS
+$.getJSON("data/users.json", function(element) {
+    var users = [];
+
+    $.each(element, function(key, val) {
+        //console.log(element[1].id)
+        //console.log(element[1].name)
+        //console.log(element[1].username)
+        // console.log(element[1].email)
+        console.log(element[0])
+
+        users.push("<div id='" + key + "'>" + val + "</div>");
+        //console.log(items)
+    });
+    $("<ul/>", {
+        "class": "my-new-list",
+        html: users.join("")
+    }).appendTo("body");
+});
+
+//GET COMMENTS USERS
+$.getJSON("data/comments.json", function(item) {
+    var comments = [];
+
+    $.each(item, function(key, val) {
+        console.log(item[0])
+
+        comments.push("<div id='" + key + "'>" + val + "</div>");
+        //console.log(items)
+    });
+    $("<ul/>", {
+        "class": "my-new-list",
+        html: comments.join("")
     }).appendTo("body");
 });
