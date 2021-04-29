190581817973d1e2d875d584ef627e49d15c9260
imalovelyghost <imalovelyghost13@gmail.com>
2021-04-29 11:19:18 +0200

    clean code here

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 //GET DATA POSTS
-//URL
 $.getJSON("https://jsonplaceholder.typicode.com/posts", function(data) {
     var posts = [];
-    //var data = JSON.stringify(data)
     $.each(data, function(key, val) {
-        //console.log(data)
-        console.log(key)
-        console.log(val)
-
         //POSTS - userId   
         let postUserId = val.userId
             //POSTS - id   
@@ -16,10 +10,7 @@ $.getJSON("https://jsonplaceholder.typicode.com/posts", function(data) {
         let postTitle = val.title
             //POSTS - body
         let postBody = val.body
-
-
-        //posts.push("<div id='" + key + "'>" + val + "</div>");
-
+            //Send the data
         posts.push("<h1>POST</h1><div class = 'blog-post' id='" + key + "'>" + " <br>postUserId> " + postUserId + " <br> postTitle> " + postTitle + " <br> postId> " + postId + "<br> postBody> " + postBody + "</div>");
     });
     $("<ul/>", {
@@ -29,24 +20,18 @@ $.getJSON("https://jsonplaceholder.typicode.com/posts", function(data) {
 });
 
 //GET DATA USERS
-
 $.getJSON("https://jsonplaceholder.typicode.com/users", function(element) {
     var users = [];
 
     $.each(element, function(key, val) {
         //USERS - id
         let userUserId = val.userId
-        console.log(val.userId)
             //USERS - username
         let userUserName = val.username
-
-        console.log(val.username)
             //USERS - email
         let userEmail = val.email
-        console.log(val.email)
-
+            //Send the data
         users.push("<h1>USERS</h1><div class = 'blog-user' id='" + key + "'>" + " <br>userUserId> " + userUserId + " <br> userUserName> " + userUserName + " <br> userEmail> " + userEmail + "</div>");
-        //console.log(items)
     });
     $("<ul/>", {
         "class": "my-user-list",
@@ -70,9 +55,8 @@ $.getJSON("https://jsonplaceholder.typicode.com/comments", function(item) {
         let commentMail = val.email
             //COMMENTS - body
         let commentBody = val.body
-
+            //Send the data
         comments.push("<h1>COMMENT</h1><div class = 'blog-comment' id='" + key + "'>" + " <br>commentPostId> " + commentPostId + " <br> commentId> " + commentId + " <br> commentName> " + commentName + " <br> commentMail> " + commentMail + " <br> commentBody> " + commentBody + "</div > ");
-        //console.log(items)
     });
     $("<ul/>", {
         "class": "my-comment-list",
