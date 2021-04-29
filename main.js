7c8bac123c8eff291acfa8abfacb4e3a2c70e6b1
imalovelyghost <imalovelyghost13@gmail.com>
2021-04-29 11:14:18 +0200

    all data in my project, from the API

--- main.js
+++ main.js
@@ -1,55 +1,81 @@
 //GET DATA POSTS
-$.getJSON("data/posts.json", function(data) {
+//URL
+$.getJSON("https://jsonplaceholder.typicode.com/posts", function(data) {
     var posts = [];
-
+    //var data = JSON.stringify(data)
     $.each(data, function(key, val) {
-        //console.log(data[1].userId)
-        //console.log(data[1].id)
-        //console.log(data[1].title)
-        //console.log(data[1].body)
-        console.log(data[0])
+        //console.log(data)
+        console.log(key)
+        console.log(val)
 
-        posts.push("<div id='" + key + "'>" + val + "</div>");
-        //console.log(items)
+        //POSTS - userId   
+        let postUserId = val.userId
+            //POSTS - id   
+        let postId = val.id
+            //POSTS - title
+        let postTitle = val.title
+            //POSTS - body
+        let postBody = val.body
+
+
+        //posts.push("<div id='" + key + "'>" + val + "</div>");
+
+        posts.push("<h1>POST</h1><div class = 'blog-post' id='" + key + "'>" + " <br>postUserId> " + postUserId + " <br> postTitle> " + postTitle + " <br> postId> " + postId + "<br> postBody> " + postBody + "</div>");
     });
     $("<ul/>", {
-        "class": "my-new-list",
+        "class": "my-post-list",
         html: posts.join("")
-    }).appendTo("body");
+    }).appendTo(".col-md-8");
 });
 
 //GET DATA USERS
-$.getJSON("data/users.json", function(element) {
+
+$.getJSON("https://jsonplaceholder.typicode.com/users", function(element) {
     var users = [];
 
     $.each(element, function(key, val) {
-        //console.log(element[1].id)
-        //console.log(element[1].name)
-        //console.log(element[1].username)
-        // console.log(element[1].email)
-        console.log(element[0])
+        //USERS - id
+        let userUserId = val.userId
+        console.log(val.userId)
+            //USERS - username
+        let userUserName = val.username
 
-        users.push("<div id='" + key + "'>" + val + "</div>");
+        console.log(val.username)
+            //USERS - email
+        let userEmail = val.email
+        console.log(val.email)
+
+        users.push("<h1>USERS</h1><div class = 'blog-user' id='" + key + "'>" + " <br>userUserId> " + userUserId + " <br> userUserName> " + userUserName + " <br> userEmail> " + userEmail + "</div>");
         //console.log(items)
     });
     $("<ul/>", {
-        "class": "my-new-list",
+        "class": "my-user-list",
         html: users.join("")
     }).appendTo("body");
 });
 
-//GET COMMENTS USERS
-$.getJSON("data/comments.json", function(item) {
+//GET COMMENTS USERS 
+
+$.getJSON("https://jsonplaceholder.typicode.com/comments", function(item) {
     var comments = [];
 
     $.each(item, function(key, val) {
-        console.log(item[0])
+        //COMMENTS - postId
+        let commentPostId = val.postId
+            //COMMENTS - id
+        let commentId = val.id
+            //COMMENTS - name
+        let commentName = val.name
+            //COMMENTS - email
+        let commentMail = val.email
+            //COMMENTS - body
+        let commentBody = val.body
 
-        comments.push("<div id='" + key + "'>" + val + "</div>");
+        comments.push("<h1>COMMENT</h1><div class = 'blog-comment' id='" + key + "'>" + " <br>commentPostId> " + commentPostId + " <br> commentId> " + commentId + " <br> commentName> " + commentName + " <br> commentMail> " + commentMail + " <br> commentBody> " + commentBody + "</div > ");
         //console.log(items)
     });
     $("<ul/>", {
-        "class": "my-new-list",
+        "class": "my-comment-list",
         html: comments.join("")
     }).appendTo("body");
 });
